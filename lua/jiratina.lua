1f1b2345df5702d761ab41818da3cb7fbc5aad9b
Alejandro Fabrega <progressandro@gmail.com>
2023-09-22 14:11:40 -0300

    Smaller window

--- lua/jiratina.lua
+++ lua/jiratina.lua
@@ -18,8 +18,8 @@ local function open_window()
 	local width = vim.api.nvim_get_option("columns")
 	local height = vim.api.nvim_get_option("lines")
 
-	local win_height = math.ceil(height * 0.8 - 4)
-	local win_width = math.ceil(width * 0.8)
+	local win_height = math.ceil(height * 0.4 - 4)
+	local win_width = math.ceil(width * 0.4)
 	local row = math.ceil((height - win_height) / 2 - 1)
 	local col = math.ceil((width - win_width) / 2)
 
